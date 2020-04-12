be2c13f573600690c86285388134d9b06e0f2030
Hatosabre <f0k7s9m5@gmail.com>
2020-04-12 12:05:20 +0900

    add base function

---
+++ src/modules/rules/rules.py
@@ -0,0 +1,29 @@
+def set_usr():
+    return
+
+def set_difficulty():
+    return
+
+def create_board():
+    return
+
+def start_game():
+    return
+
+def judge_able_put_disc():
+    return
+
+def put_disc():
+    return
+
+def turn_over_disc():
+    return
+
+def get_turn():
+    return
+
+def judge_winner():
+    return
+
+def end_game():
+    return
